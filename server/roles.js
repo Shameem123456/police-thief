2682d7e08ecaf10046b63adb96f4f85cc982ab5e
Shameem123456 <108502582+Shameem123456@users.noreply.github.com>
2026-01-15 11:37:53 +0530

    Create roles.js

---
+++ server/roles.js
@@ -0,0 +1,12 @@
+module.exports = [
+  { name: "King", score: 1000 },
+  { name: "Knight", score: 600 },
+  { name: "Minister", score: 700 },
+  { name: "Spy", score: 650 },
+  { name: "Police", score: 900 },
+  { name: "Jester", score: 400 },
+  { name: "Thief", score: 0 },
+  { name: "Queen", score: 800 },
+  { name: "Prince", score: 750 },
+  { name: "Treasurer", score: 550 }
+];
